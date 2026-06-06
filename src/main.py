17980388e325057c9d17d49ddfa3275f8e984b5e
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-06-06 22:02:23 +0530

    refactor: optimize and reduce codes form main.py

--- src/main.py
+++ src/main.py
@@ -1,58 +1,13 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from mnist_digit_classifier import DigitClassifier
+from train import Trainer
 
 
-epoches = 5
+epochs = 5
+trainer = Trainer(learning_rate=0.001)
 
 def main():
 
     print("Hello from mnist-digit-classifier!")
-    transform = transforms.ToTensor()
-    model = DigitClassifier()
-
-    train_datasets = datasets.MNIST(
-        root = "./data",
-        train = True,
-        download = True,
-        transform = transform
-    )
-
-    test_datasets = datasets.MNIST(
-        root = "./data",
-        train = False,
-        download = True,
-        transform = transform
-    )
-
-    train_loader = DataLoader(
-        dataset = train_datasets,
-        shuffle = True,
-        batch_size = 64
-    )
-
-    test_loader = DataLoader(
-        dataset = test_datasets,
-        shuffle = False,
-        batch_size = 64
-    )
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
-
-    for epoch in range(epoches):
-        model.train(mode = True)
-        total_loss = 0
-        for images, labels in train_loader:
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            total_loss += loss.item()
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        print(f"Epoch: {epoch + 1} loss: {total_loss/len(train_loader):.4f}")
+    trainer.train(epochs=epochs)
 
 
 if __name__ == "__main__":
