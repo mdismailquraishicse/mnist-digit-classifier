50adff2edfbb0581aaf8312aa5461b2da085a683
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-06-07 08:27:24 +0530

    feat: add confusion matrix and classification report in main.py and run test the code in mnist_digit_classifier.ipynb

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from data_loader import MnistDataLoader
 from mnist_digit_classifier import DigitClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
 
 class Trainer:
@@ -18,17 +19,26 @@ class Trainer:
     def train(self, epochs:int):
 
         train_loader = self.data_loader.train_loader
+        y_true = []
+        y_pred = []
         for epoch in range(epochs):
             total_loss = 0
             self.model.train()
             for images, labels in train_loader:
                 outputs = self.model(images)
+                _, predicted = torch.max(outputs, dim=1)
                 loss = self.criterion(outputs, labels)
                 total_loss += loss.item()
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                y_true.extend(labels.tolist())
+                y_pred.extend(predicted.tolist())
             print(f"Epoch: {epoch+1} loss: {total_loss/len(train_loader)}")
+        print("Classification Repost")
+        print(classification_report(y_true=y_true, y_pred=y_pred))
+        print("Confusion Metrix")
+        print(confusion_matrix(y_true=y_true, y_pred=y_pred))
 
 
     def evaluate(self):
@@ -39,6 +49,9 @@ class Trainer:
         total_loss = 0
         test_loader = self.data_loader.test_loader
 
+        y_true = []
+        y_pred = []
+
         with torch.no_grad():
             for images, labels in test_loader:
                 outputs = self.model(images)
@@ -47,6 +60,12 @@ class Trainer:
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
+                y_true.extend(labels.tolist())
+                y_pred.extend(predicted.tolist())
         accuracy = correct / total
         avg_loss = total_loss / len(test_loader)
         print(f"Test Loss: {avg_loss:.4f} Test accuracy: {accuracy:.4f}")
+        print("Classification Report")
+        print(classification_report(y_true=y_true, y_pred=y_pred))
+        print("Confusion Metrix")
+        print(confusion_matrix(y_true=y_true, y_pred=y_pred))
